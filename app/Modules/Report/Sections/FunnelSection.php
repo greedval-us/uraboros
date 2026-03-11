f6e082fdf9dd762ed73b14f2013446e4d44d5d99
Alexander <valkirblad@gmail.com>
2026-03-11 22:11:14 +0300

    greedval

--- app/Modules/Report/Sections/FunnelSection.php
+++ app/Modules/Report/Sections/FunnelSection.php
@@ -7,6 +7,7 @@ use App\Modules\Report\DTO\FullReportContextDTO;
 use App\Modules\Report\DTO\FunnelContextDTO;
 use App\Modules\Report\Helper\ChartHelper;
 use App\Modules\Report\Helper\FunnelTableHelper;
+use Illuminate\Support\Facades\Log;
 
 class FunnelSection implements PdfSectionContract
 {
@@ -29,6 +30,9 @@ class FunnelSection implements PdfSectionContract
         $funnelByDay = FunnelTableHelper::build(
             $a->ERperDay ?? []
         );
+
+        Log::info($a->ERperDay);
+        Log::info($funnelByDay);
 
         $funnelChart = ChartHelper::bar(
             $funnelByDay,
