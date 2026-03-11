39cc5a26f409bc37aa3afce485003c1939edcc85
Alexander <valkirblad@gmail.com>
2026-03-11 10:16:01 +0300

    greedval

--- app/Modules/Report/Helper/FunnelTableHelper.php
+++ app/Modules/Report/Helper/FunnelTableHelper.php
@@ -10,22 +10,23 @@ class FunnelTableHelper
     {
         $rows = [];
 
-        foreach ($ERperDay as $item) {
-            if (!is_array($item)) continue;
+        foreach ($ERperDay as $date => $metrics) {
 
-            foreach ($item as $date => $metrics) {
-                if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $date)) {
-                    continue;
-                }
-
-                $rows[] = [
-                    'day' => Carbon::parse($date)->format('d.m'),
-                    'viewRate' => round($metrics['viewRate'] ?? 0, 2),
-                    'reactionRate' => round($metrics['reactionRate'] ?? 0, 2),
-                    'commentRate' => round($metrics['commentRate'] ?? 0, 2),
-                    'erView' => round($metrics['ERview'] ?? 0, 2),
-                ];
+            if (!is_array($metrics) || empty($metrics)) {
+                continue;
             }
+
+            if (!preg_match('/^\d{4}-\d{2}-\d{2}$/', $date)) {
+                continue;
+            }
+
+            $rows[] = [
+                'day' => Carbon::parse($date)->format('d.m'),
+                'viewRate' => round($metrics['viewRate'] ?? 0, 2),
+                'reactionRate' => round($metrics['reactionRate'] ?? 0, 2),
+                'commentRate' => round($metrics['commentRate'] ?? 0, 2),
+                'erView' => round($metrics['ERview'] ?? 0, 2),
+            ];
         }
 
         return $rows;
