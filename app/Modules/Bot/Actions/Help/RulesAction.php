898a8e5545eaf57ac6558b9e33acb78c1e6dc324
Alexander <valkirblad@gmail.com>
2026-03-19 15:04:57 +0300

    greedval

--- app/Modules/Bot/Actions/Help/RulesAction.php
+++ app/Modules/Bot/Actions/Help/RulesAction.php
@@ -2,12 +2,23 @@
 
 namespace App\Modules\Bot\Actions\Help;
 
+use App\Modules\Bot\Enums\StorageKey;
+use App\Modules\Bot\Services\BotActionService;
+use App\Modules\Bot\Services\LangService;
+use App\Modules\Bot\Services\StorageService;
 use DefStudio\Telegraph\Models\TelegraphChat;
 
 class RulesAction
 {
+    public function __construct(
+        private BotActionService $bot,
+        private StorageService $storage,
+        private LangService $langService
+    ) {}
     public function handle(TelegraphChat $chat, string $lang): void
     {
-        // TODO:
+        $messageId = $this->bot->sendText($chat, $this->langService->get($lang, 'help.rules'));
+
+        $this->storage->set($chat, StorageKey::MESSAGE->value, $messageId);
     }
 }
