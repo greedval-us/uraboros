de5f12ea0fa528d08ad5e876ef2e51b3e112227b
Alexander <valkirblad@gmail.com>
2026-03-26 15:43:50 +0300

    greedval

--- app/Modules/Report/Pdf/PdfBuilder.php
+++ app/Modules/Report/Pdf/PdfBuilder.php
@@ -4,7 +4,6 @@ namespace App\Modules\Report\Pdf;
 
 use Barryvdh\DomPDF\Facade\Pdf;
 use App\Modules\Report\Contracts\PdfSectionContract;
-use App\Modules\Report\Helpers\EncodingHelper;
 
 class PdfBuilder
 {
@@ -18,13 +17,6 @@ class PdfBuilder
 
     public function build()
     {
-        // Обрабатываем данные каждой секции для обеспечения правильной кодировки UTF-8
-        $sectionsData = array_map(function (PdfSectionContract $section) {
-            $sectionData = $section->data();
-            // Убеждаемся, что все данные секции имеют правильную кодировку
-            return EncodingHelper::ensureUtf8($sectionData);
-        }, $this->sections);
-
         return Pdf::loadView('pdf.layout', [
             'sections' => $this->sections,
         ]);
